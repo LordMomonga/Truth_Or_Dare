b96dd643cc3cbb8563273fe9a8adb976cea4ea42
LordMomonga <109931065+LordMomonga@users.noreply.github.com>
2023-10-06 18:14:30 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ connection();
 // mddleware
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://truth-or-dare-aov.vercel.app', // Remplacez par le domaine de votre site front-end
+  origin: 'https://truth-or-dare-aov.vercel.ap', // Remplacez par le domaine de votre site front-end
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Autorise l'envoi de cookies et d'en-têtes d'autorisation
   optionsSuccessStatus: 204, // Répond avec un statut 204 (No Content) pour les pré-vérifications CORS
