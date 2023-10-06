99489bd1625509899329e41328476d10037af9d6
LordMomonga <109931065+LordMomonga@users.noreply.github.com>
2023-10-06 17:56:12 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,14 @@ const usersRoutes = require('./routes/user')
 connection();
 // mddleware
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: 'https://truth-or-dare-aov.vercel.app', // Remplacez par le domaine de votre site front-end
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Autorise l'envoi de cookies et d'en-têtes d'autorisation
+  optionsSuccessStatus: 204, // Répond avec un statut 204 (No Content) pour les pré-vérifications CORS
+};
+
+app.use(cors(corsOptions));
 //routes
 
 
