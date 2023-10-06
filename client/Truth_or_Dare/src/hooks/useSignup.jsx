29f7836c483cc17586855ac3c44171998804e931
LordMomonga <109931065+LordMomonga@users.noreply.github.com>
2023-10-06 17:23:34 +0100

    Update useSignup.jsx

--- client/Truth_or_Dare/src/hooks/useSignup.jsx
+++ client/Truth_or_Dare/src/hooks/useSignup.jsx
@@ -12,7 +12,7 @@ export const useSignup = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('http://localhost:3001/api/user/signup', {
+        const response = await fetch('https://truth-or-dare-qmttfhh2d-lordmomonga.vercel.app/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
 
