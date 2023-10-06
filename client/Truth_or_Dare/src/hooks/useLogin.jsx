ce926953f12d085fc2fa9fceb68522d3444c8d1b
LordMomonga <109931065+LordMomonga@users.noreply.github.com>
2023-10-06 17:43:13 +0100

    Update useLogin.jsx

--- client/Truth_or_Dare/src/hooks/useLogin.jsx
+++ client/Truth_or_Dare/src/hooks/useLogin.jsx
@@ -13,7 +13,7 @@ export const useLogin = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('https://truth-or-dare-qmttfhh2d-lordmomonga.vercel.app/api/user/login', {
+        const response = await fetch('https://truth-or-dare-flame.vercel.app/api/user/login', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
 
